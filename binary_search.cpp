fa5b638f9cebf8c2245a5e06eee2ae6c4c963c79
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2021-06-17 00:37:31 +0600

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,12 @@
+int binarySearch(int arr[], int p, int r, int num) {
+   if (p <= r) {
+      int mid = (p + r)/2;
+      if (arr[mid] == num)
+      return mid ;
+      if (arr[mid] > num)
+      return binarySearch(arr, p, mid-1, num);
+      if (arr[mid] > num)
+      return binarySearch(arr, mid+1, r, num);
+   }
+   return -1;
+}
