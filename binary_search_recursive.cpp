d71fea6042d90804e6a258c28cca99dd919bd94c
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2021-06-17 03:06:14 +0600

    Update and rename binary_search.cpp to binary_search_recursive.cpp

---
+++ binary_search_recursive.cpp
@@ -0,0 +1,22 @@
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+while (l <= r)
+{
+	int m = l + (r-l)/2;
+	if (arr[m] == x)
+		return m;
+	if (arr[m] < x)
+		l = m + 1;
+	else
+		r = m - 1;
+}
+
+return -1;
+}
+
+int main(void)
+{
+int result = binarySearch(arr, 0, n-1, x);
+return 0;
+}
