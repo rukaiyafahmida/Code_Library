f3d2aa73a5c26ffac6f42ab348046faf98135ae4
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2023-08-07 15:46:48 +0600

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+import math
+
+def binary_search(target, arr):
+    f = 0
+    b = len(arr)-1
+
+    while f<b:
+        m = math.floor((f+b)/2)
+        if arr[m]==target:
+            return m+1
+        if arr[m]<target:
+            f = m+1
+        elif arr[m]> target:
+            b = m -1
+    return "not found"
+
+
+
+print(binary_search(target=15, arr=[1,3,5,7,10,11]))
